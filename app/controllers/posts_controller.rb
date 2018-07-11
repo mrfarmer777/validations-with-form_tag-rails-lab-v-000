3caf0afa2c7cfb483b497ca75c39539f344903b9
Matt Farmer <mrfarmer777@gmail.com>
2018-07-11 18:14:03 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,7 +23,8 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    if Post.new(post_params).valid?
+    @post.assign_attributes(post_params)
+    if @post.valid?
       @post.update(post_params)
       redirect_to post_path(@post)
     else
